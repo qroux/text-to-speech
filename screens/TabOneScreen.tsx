a2ad8781ed0dd75f206f6ccde860fc7ac7316f59
qroux <quentin.roux@hotmail.fr>
2021-06-25 13:29:28 +0200

    Text to speech expo test

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,15 +1,28 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Button } from 'react-native';
+import * as Speech from 'expo-speech';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
 export default function TabOneScreen() {
+  const speak = () => {
+    const thingToSay =
+      'Tell me not, in mournful numbers, Life is but an empty dream! For the soul is dead that slumbers, And things are not what they seem. Life is real! Life is earnest! And the grave is not its goal. Dust thou art, to dust returnest, Was not spoken of the soul.';
+
+    Speech.speak(thingToSay, { language: 'en-US', rate: 0.6 });
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <View
+        style={styles.separator}
+        lightColor='#eee'
+        darkColor='rgba(255,255,255,0.1)'
+      />
+      <Button title='test' onPress={speak} />
+      <EditScreenInfo path='/screens/TabOneScreen.tsx' />
     </View>
   );
 }
