af9923094a182e2715e6c566854f448c75550e2f
qroux <quentin.roux@hotmail.fr>
2021-06-28 17:52:29 +0200

    Text to speeach working + Navigation fixed

--- navigation/BottomTabNavigator.tsx
+++ navigation/BottomTabNavigator.tsx
@@ -3,7 +3,11 @@
  * https://reactnavigation.org/docs/bottom-tab-navigator
  */
 
-import { Ionicons } from '@expo/vector-icons';
+import {
+  Ionicons,
+  MaterialCommunityIcons,
+  MaterialIcons,
+} from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
 import * as React from 'react';
@@ -21,20 +25,24 @@ export default function BottomTabNavigator() {
 
   return (
     <BottomTab.Navigator
-      initialRouteName="TabOne"
+      initialRouteName='TabOne'
       tabBarOptions={{ activeTintColor: Colors[colorScheme].tint }}>
       <BottomTab.Screen
-        name="TabOne"
+        name='TabOne'
         component={TabOneNavigator}
         options={{
-          tabBarIcon: ({ color }) => <TabBarIcon name="ios-code" color={color} />,
+          tabBarIcon: ({ color }) => (
+            <MaterialTabBarIcon name='admin-panel-settings' color={color} />
+          ),
         }}
       />
       <BottomTab.Screen
-        name="TabTwo"
+        name='TabTwo'
         component={TabTwoNavigator}
         options={{
-          tabBarIcon: ({ color }) => <TabBarIcon name="ios-code" color={color} />,
+          tabBarIcon: ({ color }) => (
+            <CommunityTabBarIcon name='account-music-outline' color={color} />
+          ),
         }}
       />
     </BottomTab.Navigator>
@@ -43,8 +51,27 @@ export default function BottomTabNavigator() {
 
 // You can explore the built-in icon families and icons on the web at:
 // https://icons.expo.fyi/
-function TabBarIcon(props: { name: React.ComponentProps<typeof Ionicons>['name']; color: string }) {
+function IoniconsTabBarIcon(props: {
+  name: React.ComponentProps<typeof Ionicons>['name'];
+  color: string;
+}) {
   return <Ionicons size={30} style={{ marginBottom: -3 }} {...props} />;
+}
+
+function CommunityTabBarIcon(props: {
+  name: React.ComponentProps<typeof MaterialCommunityIcons>['name'];
+  color: string;
+}) {
+  return (
+    <MaterialCommunityIcons size={30} style={{ marginBottom: -3 }} {...props} />
+  );
+}
+
+function MaterialTabBarIcon(props: {
+  name: React.ComponentProps<typeof MaterialIcons>['name'];
+  color: string;
+}) {
+  return <MaterialIcons size={30} style={{ marginBottom: -3 }} {...props} />;
 }
 
 // Each tab has its own navigation stack, you can read more about this pattern here:
@@ -55,7 +82,7 @@ function TabOneNavigator() {
   return (
     <TabOneStack.Navigator>
       <TabOneStack.Screen
-        name="TabOneScreen"
+        name='TabOneScreen'
         component={TabOneScreen}
         options={{ headerTitle: 'Tab One Title' }}
       />
@@ -69,7 +96,7 @@ function TabTwoNavigator() {
   return (
     <TabTwoStack.Navigator>
       <TabTwoStack.Screen
-        name="TabTwoScreen"
+        name='TabTwoScreen'
         component={TabTwoScreen}
         options={{ headerTitle: 'Tab Two Title' }}
       />
--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,24 +1,46 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, ScrollView } from 'react-native';
+import * as Speech from 'expo-speech';
 
-import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
 export default function TabTwoScreen() {
+  const renderWords = () => {
+    const text =
+      'Water Tell me not, in mournful numbers, Life is but an empty dream! For the soul is dead that slumbers, And things are not what they seem. Life is real! Life is earnest! And the grave is not its goal; Dust thou art, to dust returnest, Was not spoken of the soul. Not enjoyment, and not sorrow, Is our destined end or way; But to act, that each tomorrow Find us farther than today. Art is long, and Time is fleeting, And our hearts, though stout and brave, Still, like muffled drums, are beating Funeral marches to the grave. In the world’s broad field of battle, In the bivouac of Life, Be not like dumb, driven cattle! Be a hero in the strife! Trust no Future, howe’er pleasant! Let the dead Past bury its dead! Act,—act in the living Present! Heart within, and God o’erhead! Lives of great men all remind us We can make our lives sublime, And, departing, leave behind us Footprints on the sands of time. Footprints, that perhaps another, Sailing o’er life’s solemn main, A forlorn and shipwrecked brother, Seeing, shall take heart again. Let us, then, be up and doing, With a heart for any fate. Still achieving, still pursuing, Learn to labor and to wait.';
+    return text.split(' ').map((word) => {
+      return (
+        <Text
+          style={{
+            fontSize: 24,
+            paddingRight: 15,
+            paddingBottom: 15,
+          }}
+          key={word + Math.random()}
+          onPress={() => speak(word)}>
+          {word}{' '}
+        </Text>
+      );
+    });
+  };
+
+  const speak = (word: any) => {
+    Speech.VoiceQuality.Enhanced;
+    Speech.speak(word, { language: 'en-USA', rate: 0.7 });
+  };
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
-    </View>
+    <ScrollView>
+      <View style={styles.container}>{renderWords()}</View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    padding: 15,
   },
   title: {
     fontSize: 20,
