98b17779c87a916a2bb4a3e3ac74c6b7aeef8e40
qroux <quentin.roux@hotmail.fr>
2021-07-01 19:20:27 +0200

    expo-localization + i18n support implemented

--- screens/ProgressScreen.tsx
+++ screens/ProgressScreen.tsx
@@ -1,12 +1,23 @@
 import * as React from 'react';
 import { StyleSheet, Button, ScrollView, Linking } from 'react-native';
+import * as Localization from 'expo-localization';
+import i18n from 'i18n-js';
 
 import { Text, View } from '../components/Themed';
 
 export default function ProgressScreen() {
+  // Set the key-value pairs for the different languages you want to support.
+  i18n.translations = {
+    en: { title: 'Progression Dashboard' },
+    'fr-FR': { title: 'Tableau de bord' },
+  };
+  // Set the locale once at the beginning of your app.
+  i18n.locale = Localization.locale;
+  i18n.fallbacks = true;
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Progression DashBoard</Text>
+      <Text style={styles.title}>{i18n.t('title')}</Text>
       <View
         style={styles.separator}
         lightColor='#eee'
