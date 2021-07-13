8b5792ab04aec5ed2e88d34b47b96b7a18f81836
qroux <quentin.roux@hotmail.fr>
2021-07-13 19:44:46 +0200

    camera module implemented

--- screens/TextScreen.tsx
+++ screens/TextScreen.tsx
@@ -3,11 +3,11 @@ import { StyleSheet, ScrollView } from 'react-native';
 import * as Speech from 'expo-speech';
 
 import { Text, View } from '../components/Themed';
+import CameraTest from '../components/CameraTest';
 
 export default function TextScreen() {
   const renderWords = () => {
-    const text =
-      'Water Tell me not, in mournful numbers, Life is but an empty dream! For the soul is dead that slumbers, And things are not what they seem. Life is real! Life is earnest! And the grave is not its goal; Dust thou art, to dust returnest, Was not spoken of the soul. Not enjoyment, and not sorrow, Is our destined end or way; But to act, that each tomorrow Find us farther than today. Art is long, and Time is fleeting, And our hearts, though stout and brave, Still, like muffled drums, are beating Funeral marches to the grave. In the world’s broad field of battle, In the bivouac of Life, Be not like dumb, driven cattle! Be a hero in the strife! Trust no Future, howe’er pleasant! Let the dead Past bury its dead! Act,—act in the living Present! Heart within, and God o’erhead! Lives of great men all remind us We can make our lives sublime, And, departing, leave behind us Footprints on the sands of time. Footprints, that perhaps another, Sailing o’er life’s solemn main, A forlorn and shipwrecked brother, Seeing, shall take heart again. Let us, then, be up and doing, With a heart for any fate. Still achieving, still pursuing, Learn to labor and to wait.';
+    const text = 'Water Tell me not';
     return text.split(' ').map((word) => {
       return (
         <Text
@@ -32,6 +32,7 @@ export default function TextScreen() {
   return (
     <ScrollView>
       <View style={styles.container}>{renderWords()}</View>
+      <CameraTest />
     </ScrollView>
   );
 }
