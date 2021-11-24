971396c3cdac86d0e89efa501d9622bf4b6316c2
quentin roux <quentin.roux@hotmail.fr>
2021-11-24 21:16:39 +0100

    Section sorting by count value

--- src/components/DashboardScreen/VocabularySection.tsx
+++ src/components/DashboardScreen/VocabularySection.tsx
@@ -1,33 +1,36 @@
 import * as React from "react";
 import { StyleSheet } from "react-native";
-import { ListItem } from "react-native-elements";
+import { ListItem, Divider } from "react-native-elements";
 import { FlatList } from "react-native-gesture-handler";
 import { sharedStyles } from "../../constants/Container";
-import { View, Text } from "../Themed";
+import { View, Text, useThemeColor } from "../Themed";
 
 interface VocabularySectionProps {
   words: string[];
 }
 
 const VocabularySection = ({ words }: VocabularySectionProps) => {
+  var sortable = [];
+  for (var word in words) {
+    sortable.push([word, words[word]]);
+  }
+
+  const sortedList = sortable.sort((a, b) => b[1] - a[1]);
   return (
     <View style={sharedStyles.sectionContainer}>
       <Text style={sharedStyles.sectionTitle}>Vocabulary</Text>
       <FlatList
-        data={Object.keys(words)}
+        data={Object.keys(sortedList)}
         keyExtractor={(item) => item}
         renderItem={({ item }) => {
           return (
-            <ListItem bottomDivider>
-              <ListItem.Content>
-                <ListItem.Title>
-                  {words[item]} {item}
-                </ListItem.Title>
-              </ListItem.Content>
-            </ListItem>
-            // <Text style={styles.item}>
-            //   {words[item]} {item}
-            // </Text>
+            <View style={styles.itemContainer}>
+              <View style={styles.content}>
+                <Text style={styles.item}>{sortedList[item][0]}</Text>
+                <Text style={styles.item}>{sortedList[item][1]}</Text>
+              </View>
+              <Divider />
+            </View>
           );
         }}
       />
@@ -38,5 +41,14 @@ const VocabularySection = ({ words }: VocabularySectionProps) => {
 export default VocabularySection;
 
 const styles = StyleSheet.create({
-  item: {},
+  itemContainer: {},
+  content: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  item: {
+    fontSize: 18,
+    paddingBottom: 15,
+    paddingTop: 15,
+  },
 });
--- src/constants/Container.ts
+++ src/constants/Container.ts
@@ -12,5 +12,6 @@ export const sharedStyles = {
   sectionTitle: {
     fontWeight: "bold",
     fontSize: 34,
+    paddingBottom: 10,
   },
 };
